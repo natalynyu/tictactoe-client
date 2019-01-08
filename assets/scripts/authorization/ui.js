96abb25f9faad63dc08be49f84c3a7794cc8da4c
natalynyu <natalyn.yu@gmail.com>
2019-01-08 14:59:05 -0500

    Edit authorization UI to show/hide items
    Update UI so that the authorization fields show up accordingly.

--- assets/scripts/authorization/ui.js
+++ assets/scripts/authorization/ui.js
@@ -14,6 +14,16 @@ const onSignUpFail = () => {
 const onSignInSuccess = responseData => {
   store.user = responseData.user
   console.log('Store contains: ', store)
+  $('#createGame').show()
+  $('#sign-out').show()
+  $('#change-password').show()
+  $('#changePassHeading').show()
+  $('#signInHeading').hide()
+  $('#signUpHeading').hide()
+  $('#sign-in').hide()
+  $('#sign-up').hide()
+  $('#signup-message').hide()
+  $('#signout-message').hide()
   $('#signin-message').text('You have successfully signed in.')
 }
 const onSignInFail = () => {
@@ -30,7 +40,18 @@ const onChangePasswordFail = () => {
 // Sign Out UI
 const onSignOutSuccess = () => {
   store.user = null
+  console.log('Store contains: ', store)
   $('#signout-message').text('You have signed out successfully.')
+  $('#createGame').hide()
+  $('#changePassHeading').hide()
+  $('#change-password').hide()
+  $('.container').hide()
+  $('#gameInfo').hide()
+  $('#sign-out').hide()
+  $('#signInHeading').show()
+  $('#signUpHeading').show()
+  $('#sign-in').show()
+  $('#sign-up').show()
 }
 const onSignOutFail = () => {
   $('#signout-message').text('There was an error with signing out. Please try again.')
